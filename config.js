946ca3607e2779eb637c751602f7a7e7c55b3760
kiendao <kiendao@var-meta.com>
2022-12-03 00:13:23 +0700

    add

--- config.js
+++ config.js
@@ -1,42 +1,31 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
 var config = {
-    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
-    // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
-        "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
-        "YAMI~~",
+        "chị iu này",
+        "chúc c sinh nhật vui vẻ nhó",
+        "bên cạnh những người c iu quý",
+        "ngày càng nhiều niềm vui hơn",
+        "cũng có nhiều em học sinh ngoan ngoãn như e z",
+        "hoàn thành những ước mơ của riêng mình...",
+        "hết bài phát biểu của e rùi",
+        "à chưa hết đâu, tuần sau t4 đi aemall chơi với em nha"
     ],
-    /**
-     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
-     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
-     * 例如
-     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
-     *
-     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
-     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
-     */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
-        // "今天是你的生日": "./imgs/birthday.jpg",
+        "chị iu này": "./imgs/birthday.jpeg",
+        "chúc c sinh nhật vui vẻ nhó":"./imgs/happybirth.jpeg",
+        "bên cạnh những người c iu quý":"./imgs/friend.jpeg",
+        "ngày càng nhiều niềm vui hơn":"./imgs/fun.jpeg",
+        "cũng có nhiều em học sinh ngoan ngoãn như e z":"./imgs/student.jpeg",
+        "hoàn thành những ước mơ của riêng mình...":"./imgs/goal.jpeg",
+        "à chưa hết đâu, tuần sau t4 đi aemall chơi với em nha":"./imgs/goplay.jpeg"
     },
-    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        turn_on: "Thử ấn vào ik",
+        play: "lên nhạc nào",
+        bannar_coming: "3/12/2000",
+        balloons_flying: "mà hình như thiếu bóng bay hay là bóng em?",
+        cake_fadein: "một chút bánh nữa thì sao?",
+        light_candle: "ngọn lửa nhiệt huyết",
+        wish_message: "Happy Birthday!!!",
+        story: "Đôi ba điều thay lời muốn nói",
     }
 };
